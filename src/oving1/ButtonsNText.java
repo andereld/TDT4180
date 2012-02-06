e9c7c54aa4515a6a2d2a9a2a32931e56ad40db4c
Anders Wold Eldhuset <andereld@samfundet.no>
2012-02-06 05:23:16 +0100

    Removed unnecessary import

--- src/oving1/ButtonsNText.java
+++ src/oving1/ButtonsNText.java
@@ -1,11 +1,11 @@
 package oving1;
 
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+
 import javax.swing.ButtonGroup;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
